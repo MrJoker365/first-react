a4e96c0ca9ce73bde5e0ed51be81674b3eb5b74f
Taras <tarasstr123@gmail.com>
2023-11-11 16:46:51 +0200

    "Візуалізація віднімання/додавання числа (useState())"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import { React } from "react";
+import { React, useState } from "react";
 
 
 function App() {
+
+  const [likes, setLikes] = useState(0); // за допомогою useState можна змінювати відображення на сторінці
+                                         // useState являється масивом з двох елементів:
+                                             // 1. це є сам обєкт, переданий в useState
+                                             // 2. це функція, яка зчитує і виводить зміни на сторінку
+                                         // тому для використання цих двох елементів, використовується ДЕКОМПОЗИЦІЯ
+  
+  
+  function increment(params) {
+    setLikes(likes + 1)
+    
+  }
+
+  function decrement(params) {
+    setLikes(likes - 1)
+  }
+
+
   return (
     <div className="App">
+
+      <h1>{likes}</h1>
+
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
 
     </div>
   );
