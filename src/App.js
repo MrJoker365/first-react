c3e75733cc6edc770bcd5f1a91db5957653d2b65
Taras <tarasstr123@gmail.com>
2023-11-12 00:05:32 +0200

    Добавлення стилів / Передача аргументів через Props до новоствореного компонента

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { React, useState } from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import './styles/App.css' ;
+import PostItem from "./components/PostItem";
 
 
 function App() {
   
-// можна викликати безліч компонентів, і всі вони будуть працювати незалежно одне від одного
+// В даному випадку в PostItem передаються Props(дані), які динамічно потім будуть виводитись
   return (
     <div className="App"> 
-
-      <Counter/> {/* таким способом викликається компонент Counter.jsx */}
-
-      <Counter/> 
-      <ClassCounter/>
+  
+      {/* <PostItem value={"222"} item={{title: 0}} number={1} /> */} {/* для прикладу */}
+      
+      <PostItem post={{id: 1, title: "JavaScript", body: "Description"}} />
+      <PostItem post={{id: 2, title: "Java", body: "Description"}} />
 
     </div>
   );
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const PostItem = (props) => { // В даному випадку props - це значення, які передавались в  App.js у :
+                                //  <PostItem post={{id: 1, title: "JavaScript", body: "Description"}} />
+                                // доступ до title : {props.post.title}
+    return (
+        <div className="post">
+        <div className="post__content">
+          <strong>{props.post.id}. {props.post.title} </strong>
+          <div> 
+            {props.post.body}
+          </div>
+        </div>
+        <div className="post__btns">
+          <button>Видалити</button>
+        </div>
+      </div>
+    )
+}
+
+export default PostItem;
+
