399bfbb351b4f1a1ffc89ea4c584b618ea718f61
Taras <tarasstr123@gmail.com>
2023-11-11 21:43:52 +0200

    "керований інпут (useState())"

--- src/App.js
+++ src/App.js
@@ -9,21 +9,28 @@ function App() {
                                              // 2. це функція, яка зчитує і виводить зміни на сторінку
                                          // тому для використання цих двох елементів, використовується ДЕКОМПОЗИЦІЯ
   
+  const [value, setValue] = useState("Hello");
   
   function increment(params) {
-    setLikes(likes + 1)
+    setLikes(likes + 1) // використання 2-ї функції для зміни відображення на сторінці
     
   }
 
   function decrement(params) {
-    setLikes(likes - 1)
+    setLikes(likes - 1) // 
   }
 
 
   return (
     <div className="App">
-
       <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input 
+        type="text"
+        value={value}
+        onChange={event => setValue(event.target.value)} // при кожній зміні в полі буде викликатись setValue
+      />
+
 
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
