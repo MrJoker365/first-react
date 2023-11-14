57cdf715ae956df64e408c3082d45b81f6a40d49
Taras <tarasstr123@gmail.com>
2023-11-15 00:19:52 +0200

    Отримання інформації з дочірнього компонента (PostForm.jsx) за допомогою callback

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
 
 
 function App() {
@@ -15,62 +16,21 @@ function App() {
     {id: 2, title: "Java", body: "Description"}
   ])
 
-  // const [title, setTitle] = useState ("TiTLE")  
-  // const [body, setBody] = useState ("BODY")  
-
-  const [post, setPost] = useState({ title: "", body: ""})
 
 
-  const addNewPost = (e) => { // e -- це те ж саме що event (для себе скоротив)
-    e.preventDefault(); // вимикає дефолтне перезавантаження
+// 58 ХВ
 
-    /*
-    const newPost = {
-      id: Date.now(), // завжди має бути унікальне значення
-                      // у PostList.jsx потім передається як key={post.id} через .map
-      title,
-      body
-    }
-    setPosts([...posts, newPost])  // така концепція добавлення нового поста
-    */
-
-    // setTitle("") // Обнуляєм поля після нажимання кнопки
-    // setBody("")  //
-
-    setPosts([...posts, {...post, id: Date.now()}])
-    setPost({title: "", body: ""})
-
+  const createPost = (newPost) => { // безпосередньо сам callback
+    setPosts([...posts, newPost])
   }
-
   
   return (
     <div className="App"> 
 
-      <form>
-      
-        <MyInput 
-          // value={title}
-          // onChange={e => setTitle(e.target.value)}
-          value={post.title}
-          onChange={e => setPost({...post, title: e.target.value})} 
-          type="text" 
-          placeholder="Назва поста"
-        />
-        
-        <MyInput
-          // value={body}
-          // onChange={e => setBody(e.target.value) } 
-          value={post.body}
-          onChange={e => setPost({...post, body: e.target.value})} 
-          type="text" 
-          placeholder="Опис поста"
-        />  
+     {/* Для того, щоб отримати інформацію з дочірнього класу, потрібно створити колбек */}
+      <PostForm create={createPost}/> {/* тут буде передаватись власний колбек */}
 
-        <MyButton onClick={addNewPost} >Пост</MyButton> 
-
-        <PostList posts={posts} title="Список 1"/>
-
-      </form>
+      <PostList posts={posts} title="Список 1"/>
 
 
     </div>
---
+++ src/components/PostForm.jsx
@@ -0,0 +1,46 @@
+import { React, useState } from "react";
+import MyInput from "./UI/input/MyInput";
+import MyButton from "./UI/button/MyButton";
+
+const PostForm = ({create}) => { // деструктуризація колбеку create
+
+    const [post, setPost] = useState({ title: "", body: ""});
+
+
+    const addNewPost = (e) => { 
+        e.preventDefault(); 
+        const newPost = { // функція, яка передається до callback
+            ...post, 
+            id: Date.now()
+        }
+        create(newPost)
+        setPost({ title: "", body: ""})
+        
+    
+      }
+
+    return (
+        <form>
+      
+        <MyInput 
+          value={post.title}
+          onChange={e => setPost({...post, title: e.target.value})} 
+          type="text" 
+          placeholder="Назва поста"
+        />
+        
+        <MyInput
+          value={post.body}
+          onChange={e => setPost({...post, body: e.target.value})} 
+          type="text" 
+          placeholder="Опис поста"
+        />  
+
+        <MyButton onClick={addNewPost} >Пост</MyButton> 
+
+        
+      </form>
+    )
+}
+
+export default PostForm;
