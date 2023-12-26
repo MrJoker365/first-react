da676b87f77d5b548668b6c323452fe598565cf2
Taras <tarasstr123@gmail.com>
2023-12-26 21:30:39 +0200

    створив універсальний Navbar.jsx для <header>, сторінку з помилкою

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './styles/App.css' ;
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
+import Navbar from "./components/UI/Navbar/Navbar";
+import Error from "./pages/Error";
 
 
 
 function App() {
     return (
         <Routes>
-            <Route path="/about" element={<About />} />
-            <Route path="/" element={<Posts/>} />
+            <Route path="/" element={<Navbar />}>  {/*  буде на всіх сторінка (header, footer)  */}
+                <Route index element={<Posts/>} /> {/* index - за для уникнення помилки з path="/"  */}
+                <Route path="about" element={<About />} />
+                <Route path="*" element={<Error />} />
+            </Route>
         </Routes>
     )
 }
---
+++ src/components/UI/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Link, Outlet} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        // <div className="navbar">
+        //     <div className="navbar__links">
+        //         <Link to="/abou" > Про сайт </Link>    {/*  замість  <a href="/about">Про сайт</a>  */}
+        //         <Link to="/posts" > Пости </Link>
+        //     </div>
+        // </div>
+
+        <>
+            <header>
+                <Link to="/about" > Про сайт </Link>    {/*  замість  <a href="/about">Про сайт</a>  */}
+                <Link to="/" > Пости </Link>
+            </header>
+
+            <Outlet /> {/* буде передано весь контент */}
+
+            <footer>
+                2023
+            </footer>
+        </>
+    );
+};
+
+export default Navbar;
